727810f723ea680bc11abe14aaa0ce7f57cbc05b
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-08-26 14:51:49 +0900

    Time: 6368 ms (5.35%), Space: 71.4 MB (5.88%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -0,0 +1,16 @@
+from collections import Counter
+from itertools import permutations
+import math
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        ls = [int(''.join(i)) for i in list(permutations(str(n), len(str(n)))) if i[0] != '0']
+        poweroftwo = []
+        # 2의 거듭제곱으로 나타낼 수 있는 수의 리스트 (10e9이내)
+        for i in range(34):
+            poweroftwo.append(1<<i)
+
+        for i in ls:
+            if i in poweroftwo:
+                return True
+        else:
+            return False
