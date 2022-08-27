2a9c0557dfca5bb3c2ac876ac1433c7e7eab0463
HyeonsooKim <gustn9442@naver.com>
2022-08-27 18:59:28 +0900

    Add 1-two-sum other solutions

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -7,3 +7,25 @@ class Solution:
                     return [i,j]
                 else:
                     continue
+
+# 다른 풀이 1
+def twoSum(self, nums, target):
+    d = {}
+    for i, n in enumerate(nums):
+        m = target - n
+        if m in d:
+            return [d[m], i]
+        else:
+            d[n] = i
+
+# 다른 풀이 2
+class Solution(object):
+	def twoSum(self, nums, target):
+		buffer_dictionary = {}
+		for i in rangenums.__len()):
+			if nums[i] in buffer_dictionary:
+				return [buffer_dictionary[nums[i]], i] #if a number shows up in the dictionary already that means the 
+														#necesarry pair has been iterated on previously
+			else: # else is entirely optional
+				buffer_dictionary[target - nums[i]] = i 
+				# we insert the required number to pair with should it exist later in the list of numbers
